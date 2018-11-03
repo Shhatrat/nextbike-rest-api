80d4d3b69509c90a139da8204cbe4846fd5e6ceb
szymon <szymonkaminski.ptb@gmail.com>
2018-11-03 21:56:39 +0100

    add nearest city endpoint

--- src/main/java/com/shhatrat/nextbike/Controller.java
+++ src/main/java/com/shhatrat/nextbike/Controller.java
@@ -2,7 +2,7 @@ package com.shhatrat.nextbike;
 
 import com.shhatrat.nextbike.model.DataHolder;
 import com.shhatrat.nextbike.model.myApi.MCountry;
-import com.shhatrat.nextbike.model.myApi.MInfo;
+import com.shhatrat.nextbike.model.original.Country;
 import io.reactivex.Single;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,4 +45,21 @@ public class Controller {
                 );
     }
 
+    @GetMapping("country/nearest")
+    Single<List<MCountry>> getAllCountries(
+                                           @RequestParam(value = "lat", required = true, defaultValue = "0") double lat,
+                                           @RequestParam(value = "lng", required = true, defaultValue = "0") double lng,
+                                           @RequestParam(value = "items", required = false, defaultValue = "500") int items,
+                                           @RequestParam(value = "skip", required = false, defaultValue = "0") int skip){
+        return   DataHolder
+                .getAllData()
+                .map(m ->
+                        (m.getCountryList().stream()
+                                .sorted((country1, country2) -> Country.compareDistance(country1, country2, lat, lng) )
+                                .map(MCountry::new)
+                                .skip(skip)
+                                .limit(items)
+                        ).collect(Collectors.toList())
+                );
+    }
 }
--- src/main/java/com/shhatrat/nextbike/model/original/Country.java
+++ src/main/java/com/shhatrat/nextbike/model/original/Country.java
@@ -18,6 +18,20 @@ public class Country {
     @Attribute
     public String name;
 
+    @Attribute
+    Double lat;
+
+    @Attribute
+    Double lng;
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
     public String getName() {
         return name;
     }
@@ -26,7 +40,24 @@ public class Country {
         return cityList;
     }
 
+
     public City getCity(){
         return cityList.get(0);
+    }
+
+    public static int compareDistance(Country c1, Country c2, Double lat, Double lng){
+        Country virtualCityOfUserCoordinates = new Country();
+        virtualCityOfUserCoordinates.lat = lat;
+        virtualCityOfUserCoordinates.lng = lng;
+        return (int) (haversineDistance(c1, virtualCityOfUserCoordinates) - haversineDistance(c2, virtualCityOfUserCoordinates));
+        }
+
+    public static double haversineDistance(Country pointA, Country pointB) {
+        double deltaLat = Math.toRadians(pointB.lat - pointA.lat);
+        double deltaLong = Math.toRadians(pointB.lng - pointA.lng);
+        double a = Math.pow(Math.sin(deltaLat / 2), 2) + Math.cos(Math.toRadians(pointA.lat)) *
+                Math.cos(Math.toRadians(pointB.lat)) * Math.pow(Math.sin(deltaLong / 2), 2);
+        double greatCircleDistance = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        return 3958.761 * greatCircleDistance;
     }
 }
