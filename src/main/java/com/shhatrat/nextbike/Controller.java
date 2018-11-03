fd6716f35dd9a35cdf92f73d1833355fad114354
szymon <szymonkaminski.ptb@gmail.com>
2018-11-03 21:18:32 +0100

    add limits and skip parameter

--- src/main/java/com/shhatrat/nextbike/Controller.java
+++ src/main/java/com/shhatrat/nextbike/Controller.java
@@ -6,6 +6,7 @@ import com.shhatrat.nextbike.model.myApi.MInfo;
 import io.reactivex.Single;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,18 +22,24 @@ public class Controller {
 
 
     @GetMapping("/country/all")
-    Single<List<MCountry>> getAllCountries(){
+    Single<List<MCountry>> getAllCountries(
+            @RequestParam(value = "items", required = false, defaultValue = "500") int items,
+            @RequestParam(value = "skip", required = false, defaultValue = "0") int skip){
        return   DataHolder
                .getAllData()
                .map(m ->
                        (m.getCountryList().stream()
                                .map(MCountry::new)
+                               .skip(skip)
+                               .limit(items)
                        ).collect(Collectors.toList())
                );
     }
 
     @GetMapping("/country/{nameFromUser}")
-    Single<List<MCountry>> getAllCountries(@PathVariable String nameFromUser){
+    Single<List<MCountry>> getAllCountries(@PathVariable String nameFromUser,
+                                           @RequestParam(value = "items", required = false, defaultValue = "500") int items,
+                                           @RequestParam(value = "skip", required = false, defaultValue = "0") int skip){
         return   DataHolder
                 .getAllData()
                 .map(m ->
